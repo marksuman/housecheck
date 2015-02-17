a80fd1d11bcb909233cff381a4fa6e4c3cc7f9c7
Mark Suman <mwsuman@gmail.com>
2015-02-16 22:44:48 -0600

    Added a Watch app to the project.

---
+++ Perimeter WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Perimeter WatchKit Extension
+//
+//  Created by Mark Suman on 2/16/15.
+//  Copyright (c) 2015 Sub-Saharan. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ Perimeter WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  Perimeter WatchKit Extension
+//
+//  Created by Mark Suman on 2/16/15.
+//  Copyright (c) 2015 Sub-Saharan. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
