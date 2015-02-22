261105b9017f94c9fedf42bfa883c3b94e33bd4b
Mark Suman <mwsuman@gmail.com>
2015-02-21 23:54:57 -0600

    Add an interface object class for the AddCheckpoint interface.

---
+++ Perimeter WatchKit Extension/AddCheckpointInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  AddCheckpointInterfaceController.h
+//  CheckIt
+//
+//  Created by Mark Suman on 2/21/15.
+//  Copyright (c) 2015 Sub-Saharan. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface AddCheckpointInterfaceController : WKInterfaceController
+
+@end
---
+++ Perimeter WatchKit Extension/AddCheckpointInterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  AddCheckpointInterfaceController.m
+//  CheckIt
+//
+//  Created by Mark Suman on 2/21/15.
+//  Copyright (c) 2015 Sub-Saharan. All rights reserved.
+//
+
+#import "AddCheckpointInterfaceController.h"
+
+
+@interface AddCheckpointInterfaceController()
+
+@end
+
+
+@implementation AddCheckpointInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
