79dcae4b846f2d8ba1ce0ed84c4d0eed2880f1e1
Mark Suman <mwsuman@gmail.com>
2015-04-06 22:47:10 -0500

    Swap out the collectionviewcontroller for a regular uiviewcontroller subclass. Set it up in the storyboard.

---
+++ CheckIt/CheckpointsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CheckpointsViewController.h
+//  Perimeter
+//
+//  Created by Mark Suman on 4/6/15.
+//  Copyright (c) 2015 Sub-Saharan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckpointsViewController : UIViewController
+
+@end
