01e082cd9d5a269b9cce72b27d37b5c6d5570d7b
Mark Suman <mwsuman@gmail.com>
2015-04-09 22:24:49 -0500

    Add a one-time Completion Modal to let users know things reset after 10 hours.

---
+++ Perimeter WatchKit Extension/CompletionModalInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  CompletionModalInterfaceController.h
+//  Perimeter
+//
+//  Created by Mark Suman on 4/9/15.
+//  Copyright (c) 2015 Sub-Saharan. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface CompletionModalInterfaceController : WKInterfaceController
+
+@end
---
+++ Perimeter WatchKit Extension/CompletionModalInterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  CompletionModalInterfaceController.m
+//  Perimeter
+//
+//  Created by Mark Suman on 4/9/15.
+//  Copyright (c) 2015 Sub-Saharan. All rights reserved.
+//
+
+#import "CompletionModalInterfaceController.h"
+
+
+@interface CompletionModalInterfaceController()
+
+@end
+
+
+@implementation CompletionModalInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
